6b6bd3f72a6ac137fb4ed571d970ea9edc4d55f3
Kelvin Alves <ppsageral@gmail.com>
2020-02-01 16:48:06 -0300

    liberando o cors

--- src/main/java/com/fatecsp/agendafatecana/AgendaFatecanaApplication.java
+++ src/main/java/com/fatecsp/agendafatecana/AgendaFatecanaApplication.java
@@ -2,7 +2,12 @@ package com.fatecsp.agendafatecana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 public class AgendaFatecanaApplication {
 
@@ -10,4 +15,15 @@ public class AgendaFatecanaApplication {
 		SpringApplication.run(AgendaFatecanaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+				.allowedOrigins("*");
+			}
+		};
+	}
+	
 }
